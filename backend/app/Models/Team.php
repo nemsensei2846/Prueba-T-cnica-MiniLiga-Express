ffd560b5b7ca2c5de1d6edb3b826827355a7971d
SENSEI <inversionjean26@gmail.com>
2025-11-03 12:08:51 -0500

    Add files via upload

---
+++ backend/app/Models/Team.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Team extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'city',
+    ];
+
+    public function homeMatches()
+    {
+        return $this->hasMany(Match::class, 'home_team_id');
+    }
+
+    public function awayMatches()
+    {
+        return $this->hasMany(Match::class, 'away_team_id');
+    }
+
+    public function matches()
+    {
+        return $this->homeMatches()->union($this->awayMatches());
+    }
+}
